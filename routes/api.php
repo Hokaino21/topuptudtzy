c54e04f0ac831e43d4ecc6e6b02bbbba4ad0798e
ryfnpshy <m.rayfanpashya00@gmail.com>
2025-11-12 17:06:36 +0700

    gausah pake bearer token

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']);
-    // List all users (admin)
-    Route::get('/users', [\App\Http\Controllers\Api\UserController::class, 'index']);
-    // API endpoint to update profile including multipart file upload
-    Route::patch('/profile', [ProfileController::class, 'update']);
-});
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/user', [AuthController::class, 'user']);
+// List all users (now public)
+Route::get('/users', [\App\Http\Controllers\Api\UserController::class, 'index']);
+// API endpoint to update profile including multipart file upload
+Route::patch('/profile', [ProfileController::class, 'update']);
